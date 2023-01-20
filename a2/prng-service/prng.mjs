c35ea1ca4c45b198b113066397df06475abddd9c
Jimmy <dm.a.uvarov@gmail.com>
2023-01-20 02:41:25 -0500

    write service with generating numbers

---
+++ a2/prng-service/prng.mjs
@@ -0,0 +1,36 @@
+import { readFile } from 'fs/promises';
+import fs from 'fs';
+
+var randNumber = Math.floor(Math.random() * 3);
+
+async function read() {
+  try {
+    const data = await readFile('../pipes/prng-service.txt', { encoding: 'utf8' });
+    console.log("reading data from prtg-service.txt:",data);
+    return data
+  } catch (err) {
+    console.log(err);
+  }
+
+}
+
+async function writeRandNum(){
+  try {
+    randNumber = Math.floor(Math.random() * 3);
+    await fs.writeFile('../pipes/prng-service.txt',randNumber.toString(),{flag: 'w+'},()=>{})
+     console.log(`write random number from 0 to 2 to file prng-service.txt: ${randNumber}`)
+     console.log
+  } catch (er) {
+    console.log('error:',er.message);
+  }
+}
+
+
+async function routine(){
+  const r =  await read()
+  if (r =='run') {
+    await writeRandNum();
+  }
+}
+
+setInterval(routine, 3000);
