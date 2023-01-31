940e65284dc04c50dfc0afb4ebca55a378c9bdd0
Jimmy <dm.a.uvarov@gmail.com>
2023-01-30 22:54:29 -0500

    make skeleton of guest part

--- color-themes-observer/src/App.js
+++ color-themes-observer/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  // ============== TO DO ===============
+  // * include in render differnet components that will persist at all
+  //pages
+  // header, footer, layout, navbar?
+  // include router here 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>logo here</h1>
+        <span>Random stylized button</span>
+        <p>welcome guest!</p>
+        <button>login/logout</button>
       </header>
-    </div>
+      <main>
+        <section>
+          <article>
+            <h2> content to be changed </h2>
+            <h3>header example</h3>
+            <p>text here</p>
+          </article>
+        </section>
+        <aside>
+          <h2>menu with color schemas</h2>
+          <button>schema 1</button>
+          <button>schema 2</button>
+          <button>schema 3</button>
+        </aside>
+      </main>
+      <footer>legal info</footer>
+    </>
   );
 }
 
