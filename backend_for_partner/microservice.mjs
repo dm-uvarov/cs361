3c961f826abdff18fd023aee717639ea5f93ae86
Jimmy <dm.a.uvarov@gmail.com>
2023-02-13 16:33:08 -0500

    setup and make small server

---
+++ backend_for_partner/microservice.mjs
@@ -0,0 +1,101 @@
+
+import express from 'express';
+import 'dotenv/config';
+
+const runMSG = "run"
+const app = express();
+const PORT = process.env.PORT
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// app.use(express.static('../public'));
+
+async function WriteRunMSG(){
+  try {
+    await fs.writeFile('../pipes/prng-service.txt',runMSG,{flag: 'w+'},()=>{})
+     console.log('write "run" msg to file prng-service.txt')
+  } catch (er) {
+    console.log('error:',er.message);
+  }
+}
+
+async function writeNumberMSG(msg){
+  try {
+    await fs.writeFile('../pipes/image-service.txt',msg,{flag: 'w+'},()=>{})
+     console.log(`write ${msg} msg to file image-service.txt`)
+  } catch (er) {
+    console.log('error:',er.message);
+  }
+}
+
+async function read() {
+  try {
+    const data = await readFile('../pipes/prng-service.txt', { encoding: 'utf8' });
+    console.log("reading data from prtg-service.txt:",data);
+    return data
+  } catch (err) {
+    console.log(err);
+  }
+
+}
+
+async function readImgUrl() {
+  try {
+    const data = await readFile('../pipes/image-service.txt', { encoding: 'utf8' });
+    console.log("reading data from image-service.txt:",data);
+    return data
+  } catch (err) {
+    console.log(err);
+  }
+
+}
+var resp ;
+async function routine() {
+  const r= await read();
+
+  if (r != 'run'){
+    await writeNumberMSG(r)
+    setTimeout(()=>{
+      routine2();
+    },100)
+    
+  }
+}
+
+async function routine2() {
+  resp = await readImgUrl();
+    
+}
+
+
+// setInterval(routine,3000)
+
+// setInterval(routine2,1000)
+
+app.get("/run", (_,res)=>{
+  WriteRunMSG()
+  
+  setTimeout(()=>{routine()},60)
+  setTimeout(()=>{routine2()},60)
+
+  // var jsondata = JSON.stringify(resp)
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  // console.log('jsondata:', jsondata)
+  res.send(JSON.stringify({"image": "is processing"}));
+})
+
+app.get("/image", (_,res)=>{
+  var jsondata = JSON.stringify(resp)
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  console.log('jsondata:', jsondata)
+  res.send(jsondata);
+})
+
+
+app.listen(PORT, ()=> {
+  console.log(`Server is listening on port ${PORT}...`)
+})
--- backend_for_partner/readme.md
+++ backend_for_partner/readme.md
@@ -1,1 +1,5 @@
-# here will be place for back end for Skyelar
+#  Backend microserviceservice for Skyelar's project.
+
+service accept GET HTTP request with '/random' in URL, and return random string with random characters numbers, letters, special symbols from 1 to 255 as s respond message with code 200.
+
+## setup nev
